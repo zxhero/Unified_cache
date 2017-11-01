67c1d273dbfb941f9abe939c98559bdc97d49eb0
Xu Zhang <zhangxu415@mails.ucas.ac.cn>
2017-11-01 10:50:06 +0800

    Create parameters.h

---
+++ parameters.h
@@ -0,0 +1,64 @@
+// General
+`define NUM_INSTS_FETCH_PER_CYCLE                       4
+
+`define BYTE_LEN_IN_BITS                                8
+`define CPU_WORD_LEN_IN_BITS                            32
+`define SINGLE_INST_LEN_IN_BITS                         32
+
+`define CPU_WORD_LEN_IN_BYTES                           (`CPU_WORD_LEN_IN_BITS) / (`BYTE_LEN_IN_BITS)
+`define SINGLE_INST_LEN_IN_BYTES                        (`SINGLE_INST_LEN_IN_BITS) / (`BYTE_LEN_IN_BITS)
+`define INSTS_FETCH_WIDTH_IN_BITS                       (`NUM_INSTS_FETCH_PER_CYCLE) * (`SINGLE_INST_LEN_IN_BITS)
+`define INSTS_FETCH_WIDTH_IN_BYTES                      (`NUM_INSTS_FETCH_PER_CYCLE) * (`SINGLE_INST_LEN_IN_BYTES)
+
+// For unified cache
+`define UNIFIED_CACHE_SIZE_IN_BYTES                     1024
+`define UNIFIED_CACHE_SET_ASSOCIATIVITY                 4              
+`define UNIFIED_CACHE_BLOCK_SIZE_IN_BYTES               1
+`define UNIFIED_CACHE_NUM_SETS                          (`UNIFIED_CACHE_SIZE_IN_BYTES) / (`UNIFIED_CACHE_SET_ASSOCIATIVITY) / (`UNIFIED_CACHE_BLOCK_SIZE_IN_BYTES)
+`define UNIFIED_CACHE_BLOCK_SIZE_IN_BITS                (`UNIFIED_CACHE_BLOCK_SIZE_IN_BYTES) * 8
+`define UNIFIED_CACHE_BLOCK_OFFSET_LEN_IN_BITS          ($clog2(`UNIFIED_CACHE_BLOCK_SIZE_IN_BYTES))
+`define UNIFIED_CACHE_BLOCK_OFFSET_POS_HI               (`UNIFIED_CACHE_BLOCK_OFFSET_LEN_IN_BITS) - 1
+`define UNIFIED_CACHE_BLOCK_OFFSET_POS_LO               0
+`define UNIFIED_CACHE_INDEX_LEN_IN_BITS                 ($clog2((`UNIFIED_CACHE_SIZE_IN_BYTES) / (`UNIFIED_CACHE_BLOCK_SIZE_IN_BYTES) / (`UNIFIED_CACHE_SET_ASSOCIATIVITY)))
+`define UNIFIED_CACHE_INDEX_POS_HI                      (`UNIFIED_CACHE_BLOCK_OFFSET_LEN_IN_BITS) + (`UNIFIED_CACHE_INDEX_LEN_IN_BITS) - 1
+`define UNIFIED_CACHE_INDEX_POS_LO                      (`UNIFIED_CACHE_BLOCK_OFFSET_LEN_IN_BITS)
+`define UNIFIED_CACHE_TAG_LEN_IN_BITS                   (`CPU_WORD_LEN_IN_BITS) - (`UNIFIED_CACHE_BLOCK_OFFSET_LEN_IN_BITS) - (`UNIFIED_CACHE_INDEX_LEN_IN_BITS)
+`define UNIFIED_CACHE_TAG_POS_HI                        (`UNIFIED_CACHE_BLOCK_OFFSET_LEN_IN_BITS) + (`UNIFIED_CACHE_INDEX_LEN_IN_BITS) + (`UNIFIED_CACHE_TAG_LEN_IN_BITS) - 1
+`define UNIFIED_CACHE_TAG_POS_LO                        (`UNIFIED_CACHE_BLOCK_OFFSET_LEN_IN_BITS) + (`UNIFIED_CACHE_INDEX_LEN_IN_BITS)
+
+// unified cache packet format
+
+`define UNIFIED_CACHE_PACKET_ADDR_POS_LO                0
+`define UNIFIED_CACHE_PACKET_ADDR_POS_HI                (`UNIFIED_CACHE_PACKET_ADDR_POS_LO) + (`CPU_WORD_LEN_IN_BITS) - 1
+`define UNIFIED_CACHE_PACKET_DATA_POS_LO                (`UNIFIED_CACHE_PACKET_ADDR_POS_HI) + 1
+`define UNIFIED_CACHE_PACKET_DATA_POS_HI                (`UNIFIED_CACHE_PACKET_DATA_POS_LO) + (`UNIFIED_CACHE_BLOCK_SIZE_IN_BITS) - 1
+`define UNIFIED_CACHE_PACKET_VALID_POS                  (`UNIFIED_CACHE_PACKET_DATA_POS_HI) + 1
+`define UNIFIED_CACHE_PACKET_IS_WRITE_POS               (`UNIFIED_CACHE_PACKET_VALID_POS) + 1
+`define UNIFIED_CACHE_PACKET_TYPE_POS_LO                (`UNIFIED_CACHE_PACKET_IS_WRITE_POS) + 1
+`define UNIFIED_CACHE_PACKET_TYPE_POS_HI                (`UNIFIED_CACHE_PACKET_TYPE_POS_LO) + 1
+`define UNIFIED_CACHE_PACKET_WIDTH_IN_BITS              (`CPU_WORD_LEN_IN_BITS) + (`UNIFIED_CACHE_BLOCK_SIZE_IN_BITS) + 4
+
+`define UNIFIED_CACHE_PACKET_TYPE_NORMAL                2'b00
+`define UNIFIED_CACHE_PACKET_TYPE_MISS_REPLAY           2'b01
+
+// unified cache <==> memory packet format
+`define MEM_PACKET_WIDTH_IN_BITS                        2 + (`CPU_WORD_LEN_IN_BITS) + (`BYTE_LEN_IN_BITS)
+
+`define MEM_PACKET_ADDR_POS_LO                          0
+`define MEM_PACKET_ADDR_POS_HI                          (`MEM_PACKET_ADDR_POS_LO) + (`CPU_WORD_LEN_IN_BITS) - 1
+`define MEM_PACKET_DATA_POS_LO                          (`MEM_PACKET_ADDR_POS_HI) + 1
+`define MEM_PACKET_DATA_POS_HI                          (`UNIFIED_CACHE_PACKET_DATA_POS_LO) + (`BYTE_LEN_IN_BITS) - 1
+`define MEM_PACKET_VALID_POS                            (`UNIFIED_CACHE_PACKET_DATA_POS_HI) + 1
+`define MEM_PACKET_IS_WRITE_POS                         (`UNIFIED_CACHE_PACKET_VALID_POS) + 1
+
+
+// microarchtecture parameters for unified cache
+
+`define INST_REQUEST_QUEUE_SIZE                         2
+`define INST_REQUEST_QUEUE_PTR_WIDTH_IN_BITS            1
+
+`define DATA_REQUEST_QUEUE_SIZE                         2
+`define DATA_REQUEST_QUEUE_PTR_WIDTH_IN_BITS            1
+
+`define WRITEBACK_BUFFER_SIZE                           2
+`define WRITEBACK_BUFFER_PTR_WIDTH_IN_BITS              1
